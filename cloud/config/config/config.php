8954474c4b1bca5609d04781fea8b09a66d3998f
Quentin Burgniard <quentinburgniard@gmail.com>
2019-12-16 21:26:26 +0100

    feat(cloud): conf file

--- cloud/config/config/config.php
+++ cloud/config/config/config.php
@@ -1,18 +1,19 @@
 <?php 
 $CONFIG = array (
-  'trusted_domains' =>
-    array (
+  'dbhost' => 'cloud-db',
+  'dbname' => 'cloud',
+  'trusted_domains' => array (
     0 => 'cloud.quentinburgniard.fr',
-    1 => 'cloud.quentinburgniard.com'
+    1 => 'cloud.quentinburgniard.com',
   ),
   'knowledgebaseenabled' => false,
   'allow_user_to_change_display_name' => false,
   'mail_from_address' => 'cloud',
   'overwriteprotocol' => 'htpps',
   'config_is_read_only' => true,
-  'objectstore' => array(
+  'objectstore' => array (
     'class' => '\\OC\\Files\\ObjectStore\\S3',
-    'arguments' => array(
+    'arguments' => array (
       'bucket' => 'cloud.quentinburgniard.com',
        'autocreate' => true,
        'key'    => 'EJ39ITYZEUH5BGWDRUFY',
