30a9d8b1cb66be61072e78ee811e7ed3f726ce08
Quentin Burgniard <quentinburgniard@gmail.com>
2020-04-05 21:00:47 +0200

    feat(registry): fix performance issue

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,5 +3,4 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy
 ssh-add .travis/deploy
-ssh -t $USER@registry.quentinburgniard.com "git pull ${DIRECTORY} && docker-compose --project-directory ${DIRECTORY} up -d registry"
-ssh -t $USER@digital-leman.com "git pull ${DIRECTORY} && docker-compose --project-directory ${DIRECTORY} up -d reverse-proxy"
+ssh -t $USER@digital-leman.com "git pull ${DIRECTORY} && docker-compose --project-directory ${DIRECTORY} up -d"
