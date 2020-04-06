1d37da070881529254b048eb84078b73fca6d69a
Quentin Burgniard <quentinburgniard@gmail.com>
2020-04-06 21:44:28 +0200

    fix(ci): fix deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash	
-# https://docs.travis-ci.com/user/deployment/custom/#git	
+# https://docs.travis-ci.com/user/deployment/custom/#git
+ls .
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy
 ssh-add .travis/deploy
-ssh -t $USER@digital-leman.com "git pull ${DIRECTORY} && docker-compose --project-directory ${DIRECTORY} up -d"
+ssh -t $USER@digital-leman.com "git pull ${DIRECTORY} && docker-compose --project-directory ${DIRECTORY} up -d reverse-proxy"
