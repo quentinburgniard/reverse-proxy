9bc87d5f1fe771cde12cf46fa5c555ca058f1439
Quentin Burgniard <quentinburgniard@gmail.com>
2020-04-13 18:41:26 +0200

    feat: release new versions cloud & api

--- cron-ssl-certs.sh
+++ cron-ssl-certs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose -f docker-compose.yml up --force-recreate ssl-certs-prod
+docker-compose -f reverse-proxy/docker-compose.yml up -d --force-recreate ssl-certs-prod && docker-compose -f reverse-proxy/docker-compose.yml up -d reverse-proxy
