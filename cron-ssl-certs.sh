11b75d520f56310c03e033210e50c10451f19a35
Quentin Burgniard <quentinburgniard@gmail.com>
2020-08-18 22:54:24 +0200

    fix(cloud): version issue

--- cron-ssl-certs.sh
+++ cron-ssl-certs.sh
@@ -1,2 +1,3 @@
-#!/bin/sh
-docker-compose -f reverse-proxy/docker-compose.yml up -d --force-recreate ssl-certs-prod && docker exec reverse-proxy nginx -s reload
+#!/bin/bash
+docker restart ssl-certs-prod
+docker exec reverse-proxy nginx -s reload
