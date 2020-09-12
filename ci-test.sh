2c8161abc6be115fd8af3f6d9007a73ea4c6c194
Quentin Burgniard <quentinburgniard@gmail.com>
2020-09-12 19:40:44 +0200

    feat(ci): improve test

--- ci-test.sh
+++ ci-test.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-docker inspect --format='{{json .Config}}' reverse-proxy
+# Docker Status
+services=("api" "cloud" "cv" "discover-me" "hestia" "lavoieducoeur" "portfolio" "reverse-proxy")
+
+for service in "${services[@]}"
+do
+  running=$(docker inspect --format='{{json .State.Running}}' $service)
+  if [ $running = "false" ]; then
+    echo "Docker Status Failed"
+    exit 1
+  fi
+done
+
+# HTTP Status
+services=("cv.quentinburgniard.fr" "discover-me.quentinburgniard.fr" "lavoieducoeur.quentinburgniard.fr" "quentinburgniard.fr")
+
+#sleep 1m
+
+for service in "${services[@]}"
+do
+  status=$(curl -k -I -so /dev/null -w '%{response_code}' https://$service)
+  if [ $status != "200" ]; then
+    echo "HTTP Status Failed"
+    exit 1
+  fi
+done
