cccd5c40460729620b4a2ac85cd2c50d80a83892
Quentin Burgniard <quentinburgniard@gmail.com>
2020-09-12 15:23:18 +0200

    fix(ci): wrong server

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -3,4 +3,4 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/id_rsa
 ssh-add .travis/id_rsa
-ssh -t ${USER}@digital-leman.com "cd $DIRECTORY && git pull && docker-compose up -d reverse-proxy && docker exec reverse-proxy nginx -s reload"
+ssh -t ${USER}@digitalleman.com "cd $DIRECTORY && git pull && docker-compose up -d reverse-proxy && docker exec reverse-proxy nginx -s reload"
