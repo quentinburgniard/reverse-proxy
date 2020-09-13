cf40a11d6302dce6f9328aa7cca3a273c1e09432
Quentin Burgniard <quentinburgniard@gmail.com>
2020-09-13 18:47:46 +0200

    fix(ci): update test

--- ci-test.sh
+++ ci-test.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
+fail=$false
 # Docker Status
 services=("api" "cloud" "cv" "discover-me" "hestia" "invoice-generator" "lavoieducoeur" "portfolio" "reverse-proxy")
 
 for service in "${services[@]}"
 do
   running=$(docker inspect --format='{{json .State.Running}}' $service)
-  if [ $running = "false" ]; then
-    echo "Docker Status Failed"
-    exit 1
+  if [ "$running" = "false" ]; then
+    echo "Docker Status Failed $service"
+    $fail=$true
   fi
 done
 
 # HTTP Status
-# services=("cv.quentinburgniard.fr" "discover-me.quentinburgniard.fr" "lavoieducoeur.quentinburgniard.fr" "quentinburgniard.fr")
+services=("cv.quentinburgniard.fr" "discover-me.quentinburgniard.fr" "lavoieducoeur.quentinburgniard.fr" "quentinburgniard.fr")
 
-# sleep 1m
+#sleep 1m
 
-# for service in "${services[@]}"
-# do
-#   curl -k -I https://$service
-#   status=$(curl -k -I -so /dev/null -w '%{response_code}' https://$service)
-#   if [ $status != "200" ]; then
-#     echo "HTTP Status Failed"
-#     exit 1
-#   fi
-# done
+for service in "${services[@]}"
+do
+  status=$(curl -k -I -so /dev/null -w '%{response_code}' https://$service)
+  if [ "$status" != "200" ]; then
+    echo "HTTP Status Failed $service"
+    $fail=$true
+  fi
+done
+
+if [ $fail ]; then
+  exit 1
+fi
