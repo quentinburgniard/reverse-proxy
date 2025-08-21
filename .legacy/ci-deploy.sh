d67c8d621e5a3a978cbb6fb81f3b43aec5e40ccf
Quentin Burgniard <quentinburgniard@gmail.com>
2025-08-21 08:41:17 +0200

    feat(k8s): ci/cd, k8s foundations, lighthouse & cleanup

--- .legacy/ci-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://docs.travis-ci.com/user/deployment/custom/#git
-eval "$(ssh-agent -s)"
-chmod 600 id_rsa
-ssh-add id_rsa
-ssh -t "${USER}@digitalleman.com" "cd $DIRECTORY && git pull && docker-compose up -d reverse-proxy && docker exec reverse-proxy nginx -s reload"
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # reverse-proxy
+## Quick Start
+### 1. Create secret
+`kubectl create secret generic env --from-env-file=.env --dry-run=client -o yaml | kubectl apply -f -`
